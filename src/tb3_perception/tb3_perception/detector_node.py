363d1ed577c9d9e191af56784d19e0807f99dd64
e-cagan <emincaganapaydin@gmail.com>
2026-02-22 14:30:06 +0300

    first commit

---
+++ src/tb3_perception/tb3_perception/detector_node.py
@@ -0,0 +1,182 @@
+"""
+A module for detector node.
+"""
+
+from ultralytics import YOLO
+from cv_bridge import CvBridge
+import numpy as np
+import cv2
+
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import Image, CameraInfo
+from vision_msgs.msg import Detection2DArray
+
+
+class DetectorNode(Node):
+    """
+    A detector node which can detect objects and aruco markers.
+    """
+
+    def __init__(self):
+        super().__init__('detector_node')
+
+        # Parameters
+        self.declare_parameter('detection_mode', 'yolo')
+        self.declare_parameter('aruco_dict_type', cv2.aruco.DICT_4X4_50)
+        self.declare_parameter('yolo_model_path', '')
+        self.declare_parameter('conf_threshold', 0.4)
+
+        # Pubs / Subs
+        self.detections_pub = self.create_publisher(Detection2DArray, 'detections', 10)
+        self.detection_image_pub = self.create_publisher(Image, 'detection_image', 10)
+        #------------------------------------------------------------------------------
+        self.cam_info_sub = self.create_subscription(CameraInfo, 'camera_info', self.cam_info_callback, 10)
+        self.image_sub = self.create_subscription(Image, 'image_raw', self.image_callback, 10)
+
+        # Others
+        self.cv_bridge = CvBridge()
+        self.marker_length = 0.05  # 5cm
+        self.cam_matrix = None
+        self.dist_coeffs = None
+
+        # Check detector mode and load the corresponding detector
+        if self.get_parameter('detection_mode').value == 'yolo':
+            # Load YOLO model from path
+            self.model = YOLO(self.get_parameter('yolo_model_path').value)
+        elif self.get_parameter('detection_mode').value == 'aruco':
+            # Initialize aruco detector
+            self.dict_type = cv2.aruco.getPredefinedDictionary(self.get_parameter('aruco_dict_type').value)
+            self.detector_parameters = cv2.aruco.DetectorParameters()
+            self.aruco_detector = cv2.aruco.ArucoDetector(self.dict_type, self.detector_parameters)
+        else:
+            pass
+
+    
+    def cam_info_callback(self, msg):
+        """
+        A callback that saves camera matrix and distortion coefficients.
+        """
+
+        # Convert cam matrix to 3x3 array
+        self.cam_matrix = np.array(msg.K).reshape(3, 3)
+        self.dist_coeffs = np.array(msg.d)
+
+    
+    def image_callback(self, msg):
+        """
+        Main callback that processes images.
+        """
+
+    
+    def detect_aruco(self, img):
+        """
+        A function that detects aruco markers.
+        """
+
+        # Convert image to grayscale for reducing channel size
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Detect markers
+        corners, ids, rejected = self.aruco_detector.detectMarkers(gray)
+        detections = []
+        
+        # Check if ids exists
+        if ids is None:
+            return detections
+        
+        # Iterate trough markers
+        for corner, marker_id in zip(corners, ids):
+            # Define image points (solvePnp expects shape of (4, 2))
+            img_points = corner.reshape(4, 2)
+            
+            # Calculate bounding box
+            xs = img_points[:, 0]
+            ys = img_points[:, 1]
+            center_x = float(np.mean(xs))
+            center_y = float(np.mean(ys))
+            width = float(np.max(xs) - np.min(xs))
+            height = float(np.max(ys) - np.min(ys))
+            
+            detection = {
+                'bbox': (center_x, center_y, width, height),
+                'class_id': f'aruco_{marker_id[0]}',
+                'score': 1.0,
+            }
+            
+            # Pose estimation (if there is camera matrix)
+            if self.cam_matrix is not None and self.dist_coeffs is not None:
+                obj_points = np.array([
+                    [-self.marker_length/2,  self.marker_length/2, 0],
+                    [ self.marker_length/2,  self.marker_length/2, 0],
+                    [ self.marker_length/2, -self.marker_length/2, 0],
+                    [-self.marker_length/2, -self.marker_length/2, 0],
+                ], dtype=np.float32)
+                
+                # Apply pose estimation
+                success, rvec, tvec = cv2.solvePnP(
+                    obj_points, img_points.astype(np.float32),
+                    self.cam_matrix, self.dist_coeffs
+                )
+                
+                # Flatten the vectors
+                if success:
+                    detection['tvec'] = tvec.flatten()
+                    detection['rvec'] = rvec.flatten()
+            
+            detections.append(detection)
+        
+        return detections
+    
+
+    def detect_yolo(self, img):
+        """
+        A function that detects objects using YOLO.
+        """
+
+        # Take out the results
+        detections = []
+        results = self.model(img)
+
+        # Iterate trough results
+        for result in results:
+            for box in result.boxes:  # Every box is a detection
+                x, y, w, h = box.xywh[0].tolist()  # tensor -> python float
+                
+                # Take the most confident label and confidence
+                conf = float(box.conf[0])
+                class_id = result.names[int(box.cls[0])]
+
+                # Confidence threshold filter
+                if conf < self.get_parameter('conf_threshold').value:
+                    continue
+
+                detection = {                  # Example:
+                    'bbox': (x, y, w, h),      #    Bounding box
+                    'class_id': class_id,      #    string: "bottle"
+                    'score': conf,             #    float: 0.87
+                }
+
+                detections.append(detection)
+
+        return detections
+    
+
+def main():
+    """
+    Function that starts and ends node lifecycle.
+    """
+
+    # Start
+    rclpy.init()
+    node = DetectorNode()
+    rclpy.spin(node)
+
+    # End
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/tb3_perception_bringup/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'tb3_perception_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+
+        # Additional directories
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cagan',
+    maintainer_email='emincaganapaydin@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
